15be2f080625e4f57123235a927bef939352474b
Georg Moertl <georgmoertl@aon.at>
2018-10-24 11:53:06 +0200

    edit input features

--- check_means_1.py
+++ check_means_1.py
@@ -21,32 +21,35 @@ meansDict={}
 for i in xrange(len(means[0])):
         meansDict.update({means.dtype.names[i]: [means[0][i], means[1][i]]})
 
-iPath='/afs/hephy.at/data/gmoertl01/DeepLepton/trainfiles/v1/2016/muo/pt_15_to_inf/DYVsQCD_ptRelSorted/mini_modulo_0_trainfile_1.root'
-iFile = ROOT.TFile.Open(iPath, 'read')
-iTree = iFile.Get('tree')
-nEntries = iTree.GetEntries()
+for mean in meansDict:
+    print mean
 
-branchList = [
-'lep_pt',
-'lep_eta',
-'pfCand_neutral_ptRel_ptRelSorted',
-'pfCand_charged_ptRel_ptRelSorted',
-]
-
-
-for branch in branchList:
-    valList = []
-
-    for i in xrange(nEntries):
-        iTree.GetEntry(i)
-        val = iTree.GetLeaf(branch)
-        valList.append([])
-        for j in xrange(val.GetLen()):
-            valList[i].append((val.GetValue(j)-meansDict[branch][0])/meansDict[branch][1])
-
-    print '%s %.10f %.10f %.7e' %(branch, meansDict[branch][0], meansDict[branch][1], -meansDict[branch][0]/meansDict[branch][1])
-    for val in valList:
-        print val
-
-iFile.Close()
+#iPath='/afs/hephy.at/data/gmoertl01/DeepLepton/trainfiles/v1/2016/muo/pt_15_to_inf/DYVsQCD_ptRelSorted/mini_modulo_0_trainfile_1.root'
+#iFile = ROOT.TFile.Open(iPath, 'read')
+#iTree = iFile.Get('tree')
+#nEntries = iTree.GetEntries()
+#
+#branchList = [
+#'lep_pt',
+#'lep_eta',
+#'pfCand_neutral_ptRel_ptRelSorted',
+#'pfCand_charged_ptRel_ptRelSorted',
+#]
+#
+#
+#for branch in branchList:
+#    valList = []
+#
+#    for i in xrange(nEntries):
+#        iTree.GetEntry(i)
+#        val = iTree.GetLeaf(branch)
+#        valList.append([])
+#        for j in xrange(val.GetLen()):
+#            valList[i].append((val.GetValue(j)-meansDict[branch][0])/meansDict[branch][1])
+#
+#    print '%s %.10f %.10f %.7e' %(branch, meansDict[branch][0], meansDict[branch][1], -meansDict[branch][0]/meansDict[branch][1])
+#    for val in valList:
+#        print val
+#
+#iFile.Close()
 
