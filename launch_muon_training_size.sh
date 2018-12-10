9c32e2b5253478c527a3d21e96f45fca1e5c9ce2
Georg Moertl <georgmoertl@aon.at>
2018-12-10 12:43:53 +0100

    new launch files

--- launch_muon_training_size.sh
+++ launch_muon_training_size.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -x
 
 #select training name
-prefix='TTs_Muon1_'
+prefix='TTs_Muon2_size_'
 
 #select training data and 
 trainingDataTxtFile='/local/gmoertl/DeepLepton/TrainingData/v6/step3/2016/muo/pt_5_-1/TTs/train_muo.txt'    #txt file should contain all training files, files should be stored in the same directroy as the txt file
 trainingOutputDirectory='/local/gmoertl/DeepLepton/DeepJet_GPU/DeepJetResults'                              #training output directory must exist
 trainingDataStructure='TrainData_deepLeptons_Muons_sorted_2016'                                             #select from DeepJet/modules/datastructures/TrainData_deepLeptons.py
-trainingModelReference='Train/deepLepton_Muons_reference.py'                                                 #select from DeepJet/Train/deepLeptonXYreference.py, where the training model can be defined, define architecture in DeepJet/modules/models/convolutional_deepLepton.py and layers in DeepJet/modules/models/buildingBlocks_deepLepton.py
+trainingModelReference='Train/deepLepton_Muons_size_reference.py'                                           #select from DeepJet/Train/deepLeptonXYreference.py, where the training model can be defined, define architecture in DeepJet/modules/models/convolutional_deepLepton.py and layers in DeepJet/modules/models/buildingBlocks_deepLepton.py
 
 #select evaluation data
 EvaluationTestDataTxtFile='/local/gmoertl/DeepLepton/TrainingData/v6/step3/2016/muo/pt_5_-1/TTs/50test_muo.txt' 
