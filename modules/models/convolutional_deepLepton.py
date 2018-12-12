1db1f2a1cb90a7e47430341eceb15eaf0320b937
Georg Moertl <georgmoertl@aon.at>
2018-12-12 13:07:50 +0100

    no CNN training

--- modules/models/convolutional_deepLepton.py
+++ modules/models/convolutional_deepLepton.py
@@ -390,5 +390,51 @@ def model_deepLeptonReference_globalVarsOnly(Inputs,nclasses,nregclasses,dropout
     model = Model(inputs=Inputs, outputs=predictions)
     return model
 
+def model_deepLeptonReference_noCNN(Inputs,nclasses,nregclasses,dropoutRate=0.5,momentum=0.2):
+
+    #Batch Normalization
+    globalvars = BatchNormalization(momentum=momentum,name='globals_input_batchnorm') (Inputs[0])
+    npf    =     BatchNormalization(momentum=momentum,name='npf_input_batchnorm')     (Inputs[1])
+    cpf    =     BatchNormalization(momentum=momentum,name='cpf_input_batchnorm')     (Inputs[2])
+    ppf    =     BatchNormalization(momentum=momentum,name='ppf_input_batchnorm')     (Inputs[3])
+    epf    =     BatchNormalization(momentum=momentum,name='epf_input_batchnorm')     (Inputs[4])
+    mpf    =     BatchNormalization(momentum=momentum,name='mpf_input_batchnorm')     (Inputs[5])
+    vtx    =     BatchNormalization(momentum=momentum,name='vtx_input_batchnorm')     (Inputs[6])
+    
+    #LSTMs
+    npf = LSTM(50,go_backwards=True,implementation=2, name='npf_lstm')(npf)
+    npf = BatchNormalization(momentum=momentum,name='npflstm_batchnorm')(npf)
+    npf = Dropout(dropoutRate)(npf)
+    
+    cpf  = LSTM(150,go_backwards=True,implementation=2, name='cpf_lstm')(cpf)
+    cpf = BatchNormalization(momentum=momentum,name='cpflstm_batchnorm')(cpf)
+    cpf = Dropout(dropoutRate)(cpf)
+    
+    ppf = LSTM(50,go_backwards=True,implementation=2, name='ppf_lstm')(ppf)
+    ppf = BatchNormalization(momentum=momentum,name='ppflstm_batchnorm')(ppf)
+    ppf = Dropout(dropoutRate)(ppf)
+    
+    epf = LSTM(50,go_backwards=True,implementation=2, name='epf_lstm')(epf)
+    epf = BatchNormalization(momentum=momentum,name='epflstm_batchnorm')(epf)
+    epf = Dropout(dropoutRate)(epf)
+    
+    mpf = LSTM(50,go_backwards=True,implementation=2, name='mpf_lstm')(mpf)
+    mpf = BatchNormalization(momentum=momentum,name='mpflstm_batchnorm')(mpf)
+    mpf = Dropout(dropoutRate)(mpf)
+    
+    vtx = LSTM(150,go_backwards=True,implementation=2, name='vtx_lstm')(vtx)
+    vtx = BatchNormalization(momentum=momentum,name='vtxlstm_batchnorm')(vtx)
+    vtx = Dropout(dropoutRate)(vtx)
+    
+    #Dense Neural Network
+    x = Concatenate()( [globalvars,npf,cpf,ppf,epf,mpf,vtx])
+    x = block_deepLeptonDense(x,dropoutRate,active=True,batchnorm=True,batchmomentum=momentum)
+
+    #Output
+    lepton_pred=Dense(nclasses, activation='softmax',kernel_initializer='lecun_uniform',name='ID_pred')(x)
+    predictions = [lepton_pred]
+    model = Model(inputs=Inputs, outputs=predictions)
+    return model
+
 
 
