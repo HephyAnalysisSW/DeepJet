02687e663e517bdd1c855503d98d3ae54fe719fe
Simon Schneider <simon.schneider@oeaw.ac.at>
2019-01-21 12:03:45 +0100

    added new class to TrainData_deepLeptons.py; changes to simon_test_training.sh

--- simon_test_training.sh
+++ simon_test_training.sh
@@ -4,9 +4,10 @@
 prefix='TTs_Muon_'
 
 #select training data and 
-trainingDataTxtFile='/local/gmoertl/DeepLepton/TrainingData/v6/step3/2016/muo/pt_5_-1/TTs/50train_muo.txt'    #txt file should contain all training files, files should be stored in the same directroy as the txt file
+#trainingDataTxtFile='/local/gmoertl/DeepLepton/TrainingData/v6/step3/2016/muo/pt_5_-1/TTs/50train_muo.txt'    #txt file should contain all training files, files should be stored in the same directroy as the txt file
+trainingDataTxtFile='/local/sschneider/DeepLepton/DeepJet_GPU/TrainingData/v1/step3/2016/ele/pt_5_-1/TTs/10train_ele.txt'    #txt file should contain all training files, files should be stored in the same directroy as the txt file
 trainingOutputDirectory='/local/sschneider/DeepLepton/DeepJet_GPU/DeepJetResults'                              #training output directory must exist
-trainingDataStructure='TrainData_deepLeptons_Muons_sorted_2016'                                             #select from DeepJet/modules/datastructures/TrainData_deepLeptons.py
+trainingDataStructure='TrainData_deepLeptons_Electrons_2016_run1'                                             #select from DeepJet/modules/datastructures/TrainData_deepLeptons.py
 trainingModelReference='Train/deepLepton_Muons_reference.py'                                                 #select from DeepJet/Train/deepLeptonXYreference.py, where the training model can be defined, define architecture in DeepJet/modules/models/convolutional_deepLepton.py and layers in DeepJet/modules/models/buildingBlocks_deepLepton.py
 
 #select evaluation data
@@ -28,12 +29,12 @@ convertFromRoot.py -r ${trainingOutputDirectory}/${prefix}TrainData/snapshot.dc
 convertFromRoot.py -r ${trainingOutputDirectory}/${prefix}TrainData/snapshot.dc
 
 #2) Training:
-#python ${trainingModelReference} ${trainingOutputDirectory}/${prefix}TrainData/dataCollection.dc ${trainingOutputDirectory}/${prefix}Training
+python ${trainingModelReference} ${trainingOutputDirectory}/${prefix}TrainData/dataCollection.dc ${trainingOutputDirectory}/${prefix}Training
 #
 #3) Evaluation:
 #a) for test data
-#convertFromRoot.py --testdatafor ${trainingOutputDirectory}/${prefix}Training/trainsamples.dc -i ${EvaluationTestDataTxtFile} -o ${trainingOutputDirectory}/${prefix}TestData
-#predict.py ${trainingOutputDirectory}/${prefix}Training/KERAS_model.h5 ${trainingOutputDirectory}/${prefix}TestData/dataCollection.dc ${trainingOutputDirectory}/${prefix}EvaluationTestData
+convertFromRoot.py --testdatafor ${trainingOutputDirectory}/${prefix}Training/trainsamples.dc -i ${EvaluationTestDataTxtFile} -o ${trainingOutputDirectory}/${prefix}TestData
+predict.py ${trainingOutputDirectory}/${prefix}Training/KERAS_model.h5 ${trainingOutputDirectory}/${prefix}TestData/dataCollection.dc ${trainingOutputDirectory}/${prefix}EvaluationTestData
 #b) for train data
 #convertFromRoot.py --testdatafor ${trainingOutputDirectory}/${prefix}Training/trainsamples.dc -i ${EvaluationTrainDataTxtFile} -o ${trainingOutputDirectory}/${prefix}TestDataIsTrainData
 #predict.py ${trainingOutputDirectory}/${prefix}Training/KERAS_model.h5 ${trainingOutputDirectory}/${prefix}TestDataIsTrainData/dataCollection.dc ${trainingOutputDirectory}/${prefix}EvaluationTestDataIsTrainData
