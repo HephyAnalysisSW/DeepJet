f7b04a07f66f214c997da2c73fa28064865a7d48
Tim Brueckler <tim.brueckler@gmail.com>
2019-02-26 12:29:14 +0100

    tims update

--- modules/models/convolutional_deepLepton.py
+++ modules/models/convolutional_deepLepton.py
@@ -2,7 +2,7 @@ from keras.layers import Dense, Dropout, Flatten,Concatenate, Convolution2D, LST
 from keras.models import Model
 from keras.layers.normalization import BatchNormalization
 from keras.layers.merge import Add, Multiply
-from buildingBlocks_deepLepton import block_deepLeptonConvolutions, block_deepLeptonDense, block_deepLeptonConvolutions_selu, block_deepLeptonDense_selu, block_deepLeptonConvolutions_pooling, block_deepLeptonConvolutions_testSize, block_deepLeptonDense_testSize, block_deepLeptonDense_testSplit_sum, block_deepLeptonDense_testSplit_cands, block_deepLeptonDense_testSplit_global, block_deepLeptonDense_testSplit_selu_sum, block_deepLeptonDense_testSplit_selu_cands, block_deepLeptonDense_testSplit_selu_global #, block_SchwartzImage, block_deepFlavourBTVConvolutions
+from buildingBlocks_deepLepton import block_deepLeptonConvolutions, block_deepLeptonDense, block_deepLeptonConvolutions_selu, block_deepLeptonDense_selu, block_deepLeptonConvolutions_pooling, block_deepLeptonConvolutions_testSize, block_deepLeptonDense_testSize, block_deepLeptonDense_testSplit_sum, block_deepLeptonDense_testSplit_cands, block_deepLeptonDense_testSplit_global, block_deepLeptonDense_testSplit_selu_sum, block_deepLeptonDense_testSplit_selu_cands, block_deepLeptonDense_testSplit_selu_global, block_deepLeptonConvolutions_Tim, block_deepLeptonDense_Tim #, block_SchwartzImage, block_deepFlavourBTVConvolutions
 
 def model_deepLeptonReference(Inputs,nclasses,nregclasses,dropoutRate=0.5,momentum=0.2):
     """
@@ -657,6 +657,78 @@ def model_deepLeptonReference_noCNN(Inputs,nclasses,nregclasses,dropoutRate=0.5,
     lepton_pred=Dense(nclasses, activation='softmax',kernel_initializer='lecun_uniform',name='ID_pred')(x)
     predictions = [lepton_pred]
     model = Model(inputs=Inputs, outputs=predictions)
+    return model
+
+import keras.regularizers
+def model_deepLeptonReference_Tim(Inputs,nclasses,nregclasses,dropoutRate=0.5,momentum=0.2):
+    """
+    reference 1x1 convolutional model for 'deepLepton'
+    with recurrent layers and batch normalisation
+    standard dropout rate it 0.1
+    should be trained for flavour prediction first. afterwards, all layers can be fixed
+    that do not include 'regression' and the training can be repeated focusing on the regression part
+    (check function fixLayersContaining with invert=True)
+    """  
+    globalvars = BatchNormalization(momentum=momentum,name='globals_input_batchnorm') (Inputs[0])
+    npf    =     BatchNormalization(momentum=momentum,name='npf_input_batchnorm')     (Inputs[1])
+    cpf    =     BatchNormalization(momentum=momentum,name='cpf_input_batchnorm')     (Inputs[2])
+    ppf    =     BatchNormalization(momentum=momentum,name='ppf_input_batchnorm')     (Inputs[3])
+    epf    =     BatchNormalization(momentum=momentum,name='epf_input_batchnorm')     (Inputs[4])
+    mpf    =     BatchNormalization(momentum=momentum,name='mpf_input_batchnorm')     (Inputs[5])
+    vtx    =     BatchNormalization(momentum=momentum,name='vtx_input_batchnorm')     (Inputs[6])
+    #ptreginput = BatchNormalization(momentum=momentum,name='reg_input_batchnorm')     (Inputs[4])
+    
+    npf, cpf, ppf, epf, mpf, vtx = block_deepLeptonConvolutions_Tim(neutrals=npf,
+                                                charged=cpf,
+                                                photons=ppf,
+                                                electrons=epf,
+                                                muons=mpf,
+                                                vertices=vtx,
+                                                dropoutRate=dropoutRate,
+                                                active=True,
+                                                batchnorm=True, batchmomentum=momentum)
+    
+    
+    #
+    npf = LSTM(50,go_backwards=True,implementation=2, name='npf_lstm', kernel_regularizer=keras.regularizers.l2(0.001))(npf)
+    npf = BatchNormalization(momentum=momentum,name='npflstm_batchnorm')(npf)
+    npf = Dropout(dropoutRate)(npf)
+    
+    cpf = LSTM(150,go_backwards=True,implementation=2, name='cpf_lstm', kernel_regularizer=keras.regularizers.l2(0.001))(cpf)
+    cpf = BatchNormalization(momentum=momentum,name='cpflstm_batchnorm')(cpf)
+    cpf = Dropout(dropoutRate)(cpf)
+    
+    ppf = LSTM(50,go_backwards=True,implementation=2, name='ppf_lstm', kernel_regularizer=keras.regularizers.l2(0.001))(ppf)
+    ppf = BatchNormalization(momentum=momentum,name='ppflstm_batchnorm')(ppf)
+    ppf = Dropout(dropoutRate)(ppf)
+    
+    epf = LSTM(50,go_backwards=True,implementation=2, name='epf_lstm', kernel_regularizer=keras.regularizers.l2(0.001))(epf)
+    epf = BatchNormalization(momentum=momentum,name='epflstm_batchnorm')(epf)
+    epf = Dropout(dropoutRate)(epf)
+    
+    mpf = LSTM(50,go_backwards=True,implementation=2, name='mpf_lstm', kernel_regularizer=keras.regularizers.l2(0.001))(mpf)
+    mpf = BatchNormalization(momentum=momentum,name='mpflstm_batchnorm')(mpf)
+    mpf = Dropout(dropoutRate)(mpf)
+    
+    vtx = LSTM(150,go_backwards=True,implementation=2, name='vtx_lstm', kernel_regularizer=keras.regularizers.l2(0.001))(vtx)
+    vtx = BatchNormalization(momentum=momentum,name='vtxlstm_batchnorm')(vtx)
+    vtx = Dropout(dropoutRate)(vtx)
+    
+    
+    x = Concatenate()( [globalvars,npf,cpf,ppf,epf,mpf,vtx])
+    
+    x = block_deepLeptonDense_Tim(x,dropoutRate,active=True,batchnorm=True,batchmomentum=momentum)
+    
+    lepton_pred=Dense(nclasses, activation='softmax',kernel_initializer='lecun_uniform',name='ID_pred')(x)
+    
+    #reg = Concatenate()( [flavour_pred, ptreginput ] ) 
+    
+    #reg_pred=Dense(nregclasses, activation='linear',kernel_initializer='ones',name='regression_pred',trainable=True)(reg)
+    
+    predictions = [lepton_pred]
+    #predictions = [flavour_pred,reg_pred]
+    model = Model(inputs=Inputs, outputs=predictions)
+    #model.save("/local/gmoertl/DeepLepton/DeepJet_GPU/DeepJet/KERAS_initial_model.h5")
     return model
 
 
