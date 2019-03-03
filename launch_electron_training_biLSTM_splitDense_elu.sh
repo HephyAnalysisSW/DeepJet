d57ed0fa55eb0c15c964f952fd85fdc406bdce85
Simon Schneider <simon.schneider@oeaw.ac.at>
2019-03-03 07:17:05 +0100

    added elu training for electrons

--- launch_electron_training_biLSTM_splitDense_elu.sh
+++ launch_electron_training_biLSTM_splitDense_elu.sh
@@ -2,13 +2,13 @@
 
 #select training name
 #prefix='TTs_Muon_'
-prefix='TTJets_Electron_biLSTM_' #produces error if not ending with '_'
+prefix='TTJets_Electron_biLSTM_splitDense_elu_' #produces error if not ending with '_'
 
 #select training data and 
 trainingDataTxtFile='/local/sschneider/DeepLepton/DeepJet_GPU/TrainingData/v1/2016/ele/pt_5_-1/TTJets/train_ele.txt'    #txt file should contain all training files, files should be stored in the same directroy as the txt file
 trainingOutputDirectory='/local/sschneider/DeepLepton/DeepJet_GPU/DeepJetResults'                              #training output directory must exist
 trainingDataStructure='TrainData_deepLeptons_Electrons_2016_run1'                                             #select from DeepJet/modules/datastructures/TrainData_deepLeptons.py
-trainingModelReference='Train/deepLepton_Electrons_biLSTM_reference.py'                                                 #select from DeepJet/Train/deepLeptonXYreference.py, where the training model can be defined, define architecture in DeepJet/modules/models/convolutional_deepLepton.py and layers in DeepJet/modules/models/buildingBlocks_deepLepton.py
+trainingModelReference='Train/deepLepton_Electrons_biLSTM_splitDense_elu_reference.py'                                                 #select from DeepJet/Train/deepLeptonXYreference.py, where the training model can be defined, define architecture in DeepJet/modules/models/convolutional_deepLepton.py and layers in DeepJet/modules/models/buildingBlocks_deepLepton.py
 
 #select evaluation data
 EvaluationTestDataTxtFile='/local/sschneider/DeepLepton/DeepJet_GPU/TrainingData/v1/2016/ele/pt_5_-1/TTJets/test_ele.txt' 
@@ -23,13 +23,13 @@ ulimit -m unlimited; ulimit -v unlimited
 
 #1) Preperation:
 # Conversion to Data Structure
-#convertFromRoot.py -i ${trainingDataTxtFile} -o ${trainingOutputDirectory}/${prefix}TrainData -c ${trainingDataStructure}
+convertFromRoot.py -i ${trainingDataTxtFile} -o ${trainingOutputDirectory}/${prefix}TrainData -c ${trainingDataStructure}
 # ensure, that all files have been converted
-#convertFromRoot.py -r ${trainingOutputDirectory}/${prefix}TrainData/snapshot.dc
-#convertFromRoot.py -r ${trainingOutputDirectory}/${prefix}TrainData/snapshot.dc
+convertFromRoot.py -r ${trainingOutputDirectory}/${prefix}TrainData/snapshot.dc
+convertFromRoot.py -r ${trainingOutputDirectory}/${prefix}TrainData/snapshot.dc
 
 #2) Training:
-python ${trainingModelReference} ${trainingOutputDirectory}/${prefix}TrainData/dataCollection.dc ${trainingOutputDirectory}/${prefix}Training
+#python ${trainingModelReference} ${trainingOutputDirectory}/${prefix}TrainData/dataCollection.dc ${trainingOutputDirectory}/${prefix}Training
 #
 #3) Evaluation:
 #a) for test data
